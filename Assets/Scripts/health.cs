cdb1b8c551a95f0ca1c98d1b885d0e3dbc44e0b9
Jamie-Jack Gidley <44707154+Jamie-Jack@users.noreply.github.com>
2018-11-09 11:35:06 +0000

    First Upload

---
+++ Assets/Scripts/health.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class health : MonoBehaviour {
+	public Slider HealthBar;
+
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player")){
+			HealthBar.value += 0.1f;
+			Destroy (this.gameObject);
+		}
+	}
+
+
+	void Start () {
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+
+	}
+}
